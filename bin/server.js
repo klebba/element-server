daf447032ad1ea7a5b4c96239961d582001322bc
Casey Klebba <cklebba@netflix.com>
2017-10-25 17:48:36 -0700

    Simplify

--- bin/server.js
+++ bin/server.js
@@ -2,11 +2,13 @@
 
 const server = require('../index.js');
 
+const host = process.env.SERVER_HOST || '0.0.0.0';
 const path = process.env.SERVER_PATH || process.argv[2] || '.';
 const port = process.env.SERVER_PORT || process.argv[3] || 8080;
+const name = process.env.npm_package_name;
 
-server(path, port, address => {
+server(name, path, port, host, address => {
   /* eslint-disable no-console */
-  console.log(`HTTP server running at http://localhost:${address.port}`);
+  console.log(`http://${host}:${address.port}/node_modules/${name}/`);
   /* eslint-enable no-console */
 });
--- index.js
+++ index.js
@@ -1,14 +1,36 @@
-const Koa = require('koa');
-const serve = require('koa-static');
-const mount = require('koa-mount');
+const st = require('node-static');
+const http = require('http');
 
-function server(path, port, callback) {
-  const app = new Koa();
+function server(name, path, port, host, callback) {
+  const fileServer = new st.Server(path, {
+    cache: 0,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET',
+      'Access-Control-Allow-Headers': 'Content-Type',
+    },
+  });
 
-  app.use(serve(path));
-  app.use(mount('/', serve(`${path}/node_modules`)));
-
-  app.listen(port, function() {
+  // prettier-ignore
+  http.createServer((request, response) => {
+    request.addListener('end', () => {
+      const base = `/node_modules/${name}`;
+      const forward = request.url.startsWith('/node_modules') === false;
+      const file = request.url.replace(base, '');
+      if (forward) {
+        response.writeHead(302, {'Location': `/node_modules/${name}${file}`});
+        response.end();
+      } else {
+        fileServer.serve(request, response, (err, result) => {
+          // alias /node_modules/module-name to /
+          if (err && err.status === 404) {
+            request.url = file;
+            fileServer.serve(request, response);
+          }
+        });
+      }
+    }).resume();
+  }).listen(port, host, function() {
     if (callback instanceof Function) {
       callback(this.address());
     }
