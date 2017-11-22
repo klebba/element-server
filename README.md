2340f083463da8349896fa7bd2b761f7c67819b2
Casey Klebba <cklebba@netflix.com>
2017-11-22 12:14:12 -0800

    Use yarn

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 It mounts `/${npm_package_name}` at `/node_modules/${npm_package_name}` to emulate the installed path of your element.
 
 ```
-npm install @klebba/element-server --save-dev
-npm run element-server
+yarn install @klebba/element-server --save-dev
+yarn run element-server
 ```
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const st = require('node-static');
 const http = require('http');
 
-function server(name, path, port, host, callback) {
+function createServer(name, path, port, host, callback) {
   const fileServer = new st.Server(path, {
     cache: 0,
     headers: {
@@ -9,32 +9,38 @@ function server(name, path, port, host, callback) {
       'Access-Control-Allow-Methods': 'GET',
       'Access-Control-Allow-Headers': 'Content-Type',
     },
+    serverInfo: `element:${name}`,
   });
 
-  // prettier-ignore
-  http.createServer((request, response) => {
-    request.addListener('end', () => {
-      const base = `/node_modules/${name}`;
-      const forward = request.url.startsWith('/node_modules') === false;
-      const file = request.url.replace(base, '');
-      if (forward) {
-        response.writeHead(302, {'Location': `/node_modules/${name}${file}`});
-        response.end();
-      } else {
-        fileServer.serve(request, response, (err, result) => {
-          // alias /node_modules/module-name to /
-          if (err && err.status === 404) {
-            request.url = file;
-            fileServer.serve(request, response);
-          }
-        });
-      }
-    }).resume();
-  }).listen(port, host, function() {
+  const server = http.createServer();
+  const base = `/node_modules/${name}`;
+
+  server.on('request', (request, response) => {
+    request
+      .addListener('end', () => {
+        const forward = request.url.startsWith('/node_modules') === false;
+        const file = request.url.replace(base, '');
+        if (forward) {
+          response.writeHead(302, { Location: `/node_modules/${name}${file}` });
+          response.end();
+        } else {
+          fileServer.serve(request, response, (err, result) => {
+            // alias /node_modules/module-name to /
+            if (err && err.status === 404) {
+              request.url = file;
+              fileServer.serve(request, response);
+            }
+          });
+        }
+      })
+      .resume();
+  });
+
+  server.listen(port, host, function() {
     if (callback instanceof Function) {
       callback(this.address());
     }
   });
 }
 
-module.exports = server;
+module.exports = createServer;
